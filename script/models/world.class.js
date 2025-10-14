206ee5320c606af92ec73f25583080bed73eb7b2
jthiel <jthiel@dsoftworks.de>
2025-10-14 18:30:36 +0200

    new Function  to draw addet

--- script/models/world.class.js
+++ script/models/world.class.js
@@ -12,9 +12,8 @@ class World {
         this.draw();
     }
 
-
-
     draw() {
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width)
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width);
+        this.ctx.drawImage(this.enemies.img, this.enemies.x, this.enemies.y, this.enemies.height, this.enemies.width);
     }
 }
