e57777fb8efe8c26b3bb4f13bf79a8a8793d177a
jthiel <jthiel@dsoftworks.de>
2025-11-02 12:27:33 +0100

    code commented out

--- script/models/world.class.js
+++ script/models/world.class.js
@@ -14,6 +14,6 @@ class World {
 
     draw() {
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width);
-        this.ctx.drawImage(this.enemies.img, this.enemies.x, this.enemies.y, this.enemies.height, this.enemies.width);
+         // this.ctx.drawImage(this.enemies.img, this.enemies.x, this.enemies.y, this.enemies.height, this.enemies.width);
     }
 }
