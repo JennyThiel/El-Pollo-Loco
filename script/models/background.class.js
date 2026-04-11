f8ed5f77e4b412259feacc727598945af366a5b0
jthiel <jthiel@dsoftworks.de>
2026-04-11 21:36:30 +0200

    Background added

---
+++ script/models/background.class.js
@@ -0,0 +1,10 @@
+class Background extends MovableObject {
+    y = 0;
+    x = 0;
+    height = 200;
+    width = 500;
+
+    constructor() {
+        super().loadImage('img/5_background/layers/1_first_layer/1.png');
+    }
+}
