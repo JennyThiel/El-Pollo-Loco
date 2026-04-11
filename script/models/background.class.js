218e6865b1013f22962d3dfd5fce78e3f76bdb0f
jthiel <jthiel@dsoftworks.de>
2026-04-11 21:58:48 +0200

    Background placed in the correct position

--- script/models/background.class.js
+++ script/models/background.class.js
@@ -1,8 +1,8 @@
 class Background extends MovableObject {
-    y = 0;
+    y = 280;
     x = 0;
     height = 200;
-    width = 500;
+    width = 720;
 
     constructor() {
         super().loadImage('img/5_background/layers/1_first_layer/1.png');
