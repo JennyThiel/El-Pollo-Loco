923b1b17e0602ee1e7abcafaf24313bf61681404
jthiel <jthiel@dsoftworks.de>
2026-08-24 18:02:14 +0200

    Consistent colors for all status bars

--- script/models/bottle.class.js
+++ script/models/bottle.class.js
@@ -3,6 +3,22 @@ class Bottle extends MovableObject {
     width = 80;
     height = 50;
 
+    // IMAGES_ROTATION_BOTTLE = [
+    //     'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+    //     'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+    //     'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+    //     'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
+    // ]
+
+    // IMAGES_SPLASH_BOTTLE = [
+    //     'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+    //     'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+    //     'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+    //     'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+    //     'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+    //     'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
+    // ]
+
     constructor(imagePath, x = Math.random() * 300) {
         super().loadImage(imagePath);
         this.x = x + 150;
